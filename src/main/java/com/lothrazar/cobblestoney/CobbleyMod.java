87e2df4781d940ca1a7991143bdfa90a68f77215
lothrazar <samson.bassett@gmail.com>
2023-04-12 10:35:16 -0700

    1.19.4 worldgen balancing

--- src/main/java/com/lothrazar/cobblestoney/CobbleyMod.java
+++ src/main/java/com/lothrazar/cobblestoney/CobbleyMod.java
@@ -7,7 +7,6 @@ import com.lothrazar.cobblestoney.registry.ConfigManager;
 import com.lothrazar.cobblestoney.registry.ItemRegistry;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CobbleyMod.MODID)
@@ -21,8 +20,9 @@ public class CobbleyMod {
     BlockRegistry.BLOCKS.register(eventBus);
     ItemRegistry.ITEMS.register(eventBus);
     ConfigManager.setup();
-    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
   }
-
-  private void setup(final FMLCommonSetupEvent event) {}
+  //worldgen defaults:
+  //most are everywhere (zero above_bottom and below_top
+  //below zero only: calcite, tuff, dripstone,   with max absolute zero
+  //the six SOFT ones have above_bottom > 0 so not all the way down
 }
