513874c4ef2526210b85b6d39aa77f985c7a43c1
Sam <lothrazar@hotmail.com>
2026-08-10 18:11:44 -0700

    porting 26.1 recipes

--- src/main/java/com/lothrazar/cobblestoney/registry/ItemRegistry.java
+++ src/main/java/com/lothrazar/cobblestoney/registry/ItemRegistry.java
@@ -17,28 +17,28 @@ public class ItemRegistry {
   public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ModCobbley.MODID);
 
 
-  public static final DeferredItem<Item> SILT = ITEMS.register("silt", () -> new ItemFlib(new Item.Properties()));
-  public static final DeferredItem<Item> LOAM = ITEMS.register("loam", () -> new ItemFlib(new Item.Properties()));
-  public static final DeferredItem<Item> MUD = ITEMS.register("mud", () -> new ItemFlib(new Item.Properties()));
+  public static final DeferredItem<Item> SILT = ITEMS.registerItem("silt", props -> new ItemFlib(props));
+  public static final DeferredItem<Item> LOAM = ITEMS.registerItem("loam", props -> new ItemFlib(props));
+  public static final DeferredItem<Item> MUD = ITEMS.registerItem("mud", props -> new ItemFlib(props));
   //
-  public static final DeferredItem<Item> DRIPSTONE_COBBLED = ITEMS.register("dripstone_cobbled", () -> new BlockItemFlib(BlockRegistry.DRIPSTONE_COBBLED.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> CALCITE_COBBLED = ITEMS.register("calcite_cobbled", () -> new BlockItemFlib(BlockRegistry.CALCITE_COBBLED.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> TUFF_COBBLED = ITEMS.register("tuff_cobbled", () -> new BlockItemFlib(BlockRegistry.TUFF_COBBLED.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> DIORITE_COBBLED = ITEMS.register("diorite_cobbled", () -> new BlockItemFlib(BlockRegistry.DIORITE_COBBLED.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> ANDESITE_COBBLED = ITEMS.register("andesite_cobbled", () -> new BlockItemFlib(BlockRegistry.ANDESITE_COBBLED.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> GRANITE_COBBLED = ITEMS.register("granite_cobbled", () -> new BlockItemFlib(BlockRegistry.GRANITE_COBBLED.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> GRANITE_SOLADITE = ITEMS.register("granite_soladite", () -> new BlockItemFlib(BlockRegistry.GRANITE_SOLADITE.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> GRANITE_JADE = ITEMS.register("granite_jade", () -> new BlockItemFlib(BlockRegistry.GRANITE_JADE.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> GRANITE_CECILIA = ITEMS.register("granite_cecilia", () -> new BlockItemFlib(BlockRegistry.GRANITE_CECILIA.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> GRANITE_RED = ITEMS.register("granite_red", () -> new BlockItemFlib(BlockRegistry.GRANITE_RED.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> GRANITE_PINK = ITEMS.register("granite_pink", () -> new BlockItemFlib(BlockRegistry.GRANITE_PINK.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> GRANITE_PURPLE = ITEMS.register("granite_purple", () -> new BlockItemFlib(BlockRegistry.GRANITE_PURPLE.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> STONE_SILT = ITEMS.register("stone_silt", () -> new BlockItemFlib(BlockRegistry.STONE_SILT.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> STONE_MUD = ITEMS.register("stone_mud", () -> new BlockItemFlib(BlockRegistry.STONE_MUD.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> DIRT_SAND = ITEMS.register("dirt_sand", () -> new BlockItemFlib(BlockRegistry.DIRT_SAND.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> DIRT_CLAY = ITEMS.register("dirt_clay", () -> new BlockItemFlib(BlockRegistry.DIRT_CLAY.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> CLAY_SAND = ITEMS.register("clay_sand", () -> new BlockItemFlib(BlockRegistry.CLAY_SAND.get(), new Item.Properties().useBlockDescriptionPrefix()));
-  public static final DeferredItem<Item> CLAY_SILT = ITEMS.register("clay_silt", () -> new BlockItemFlib(BlockRegistry.CLAY_SILT.get(), new Item.Properties().useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> DRIPSTONE_COBBLED = ITEMS.registerItem("dripstone_cobbled", props -> new BlockItemFlib(BlockRegistry.DRIPSTONE_COBBLED.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> CALCITE_COBBLED = ITEMS.registerItem("calcite_cobbled", props -> new BlockItemFlib(BlockRegistry.CALCITE_COBBLED.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> TUFF_COBBLED = ITEMS.registerItem("tuff_cobbled", props -> new BlockItemFlib(BlockRegistry.TUFF_COBBLED.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> DIORITE_COBBLED = ITEMS.registerItem("diorite_cobbled", props -> new BlockItemFlib(BlockRegistry.DIORITE_COBBLED.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> ANDESITE_COBBLED = ITEMS.registerItem("andesite_cobbled", props -> new BlockItemFlib(BlockRegistry.ANDESITE_COBBLED.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> GRANITE_COBBLED = ITEMS.registerItem("granite_cobbled", props -> new BlockItemFlib(BlockRegistry.GRANITE_COBBLED.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> GRANITE_SOLADITE = ITEMS.registerItem("granite_soladite", props -> new BlockItemFlib(BlockRegistry.GRANITE_SOLADITE.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> GRANITE_JADE = ITEMS.registerItem("granite_jade", props -> new BlockItemFlib(BlockRegistry.GRANITE_JADE.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> GRANITE_CECILIA = ITEMS.registerItem("granite_cecilia", props -> new BlockItemFlib(BlockRegistry.GRANITE_CECILIA.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> GRANITE_RED = ITEMS.registerItem("granite_red", props -> new BlockItemFlib(BlockRegistry.GRANITE_RED.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> GRANITE_PINK = ITEMS.registerItem("granite_pink", props -> new BlockItemFlib(BlockRegistry.GRANITE_PINK.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> GRANITE_PURPLE = ITEMS.registerItem("granite_purple", props -> new BlockItemFlib(BlockRegistry.GRANITE_PURPLE.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> STONE_SILT = ITEMS.registerItem("stone_silt", props -> new BlockItemFlib(BlockRegistry.STONE_SILT.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> STONE_MUD = ITEMS.registerItem("stone_mud", props -> new BlockItemFlib(BlockRegistry.STONE_MUD.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> DIRT_SAND = ITEMS.registerItem("dirt_sand", props -> new BlockItemFlib(BlockRegistry.DIRT_SAND.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> DIRT_CLAY = ITEMS.registerItem("dirt_clay", props -> new BlockItemFlib(BlockRegistry.DIRT_CLAY.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> CLAY_SAND = ITEMS.registerItem("clay_sand", props -> new BlockItemFlib(BlockRegistry.CLAY_SAND.get(), props.useBlockDescriptionPrefix()));
+  public static final DeferredItem<Item> CLAY_SILT = ITEMS.registerItem("clay_silt", props -> new BlockItemFlib(BlockRegistry.CLAY_SILT.get(), props.useBlockDescriptionPrefix()));
 
   public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ModCobbley.MODID);
 
